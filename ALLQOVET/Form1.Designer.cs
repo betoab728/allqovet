d51c701226dc6ff8de27f848b80fe2ff6e4ceaec
betoab728 <betoab728@gmail.com>
2023-05-11 13:00:00 -0500

    Iniciando el proyecto allqovet, esta es la primera subida al repositorio

---
+++ ALLQOVET/Form1.Designer.cs
@@ -0,0 +1,358 @@
+﻿namespace ALLQOVET
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            panel2 = new Panel();
+            linkLabel1 = new LinkLabel();
+            label9 = new Label();
+            label8 = new Label();
+            label7 = new Label();
+            button1 = new Button();
+            panel4 = new Panel();
+            textBox2 = new TextBox();
+            pictureBox3 = new PictureBox();
+            panel3 = new Panel();
+            textBox1 = new TextBox();
+            pictureBox2 = new PictureBox();
+            label6 = new Label();
+            cerrarbtn = new Button();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel2.SuspendLayout();
+            panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(153, 128, 154);
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(300, 530);
+            panel1.TabIndex = 0;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Century Gothic", 8F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.ForeColor = Color.White;
+            label5.Location = new Point(157, 472);
+            label5.Name = "label5";
+            label5.Size = new Size(94, 16);
+            label5.TabIndex = 5;
+            label5.Text = "curso integrador";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Century Gothic", 8F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.ForeColor = Color.White;
+            label4.Location = new Point(156, 456);
+            label4.Name = "label4";
+            label4.Size = new Size(95, 16);
+            label4.TabIndex = 4;
+            label4.Text = "Desarrollado por";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Century Gothic", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.ForeColor = Color.White;
+            label3.Location = new Point(154, 270);
+            label3.Name = "label3";
+            label3.Size = new Size(97, 24);
+            label3.TabIndex = 3;
+            label3.Text = "Allqovet";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Century Gothic", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(80, 237);
+            label2.Name = "label2";
+            label2.Size = new Size(171, 24);
+            label2.TabIndex = 2;
+            label2.Text = "Sistema Integral";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Century Gothic", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(103, 203);
+            label1.Name = "label1";
+            label1.Size = new Size(148, 24);
+            label1.TabIndex = 1;
+            label1.Text = "Bienvenido al";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.Picsart_23_05_10_23_43_44_179;
+            pictureBox1.Location = new Point(95, 40);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(120, 120);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(linkLabel1);
+            panel2.Controls.Add(label9);
+            panel2.Controls.Add(label8);
+            panel2.Controls.Add(label7);
+            panel2.Controls.Add(button1);
+            panel2.Controls.Add(panel4);
+            panel2.Controls.Add(panel3);
+            panel2.Controls.Add(label6);
+            panel2.Controls.Add(cerrarbtn);
+            panel2.Dock = DockStyle.Fill;
+            panel2.Location = new Point(300, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(450, 530);
+            panel2.TabIndex = 1;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.LinkColor = Color.FromArgb(95, 49, 96);
+            linkLabel1.Location = new Point(209, 467);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(60, 15);
+            linkLabel1.TabIndex = 9;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "linkLabel1";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Century Gothic", 8F, FontStyle.Regular, GraphicsUnit.Point);
+            label9.ForeColor = Color.White;
+            label9.Location = new Point(38, 472);
+            label9.Name = "label9";
+            label9.Size = new Size(95, 16);
+            label9.TabIndex = 8;
+            label9.Text = "Desarrollado por";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Century Gothic", 8F, FontStyle.Regular, GraphicsUnit.Point);
+            label8.ForeColor = Color.White;
+            label8.Location = new Point(38, 456);
+            label8.Name = "label8";
+            label8.Size = new Size(95, 16);
+            label8.TabIndex = 7;
+            label8.Text = "Desarrollado por";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Century Gothic", 8F, FontStyle.Regular, GraphicsUnit.Point);
+            label7.ForeColor = Color.Silver;
+            label7.Location = new Point(38, 430);
+            label7.Name = "label7";
+            label7.Size = new Size(49, 16);
+            label7.TabIndex = 6;
+            label7.Text = "Soporte";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(95, 49, 96);
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Century Gothic", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(100, 327);
+            button1.Name = "button1";
+            button1.Size = new Size(248, 35);
+            button1.TabIndex = 5;
+            button1.Text = "Ingresar";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // panel4
+            // 
+            panel4.BackColor = SystemColors.Control;
+            panel4.Controls.Add(textBox2);
+            panel4.Controls.Add(pictureBox3);
+            panel4.Location = new Point(0, 243);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(450, 45);
+            panel4.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = SystemColors.Control;
+            textBox2.BorderStyle = BorderStyle.None;
+            textBox2.Font = new Font("Century Gothic", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox2.ForeColor = Color.FromArgb(95, 49, 96);
+            textBox2.Location = new Point(55, 11);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(370, 17);
+            textBox2.TabIndex = 6;
+            textBox2.UseSystemPasswordChar = true;
+            textBox2.Click += textBox2_Click;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = Properties.Resources._622405_lock_security_password_protect_safety_icon;
+            pictureBox3.Location = new Point(15, 11);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(24, 24);
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex = 6;
+            pictureBox3.TabStop = false;
+            pictureBox3.Click += pictureBox3_Click;
+            pictureBox3.MouseDown += pictureBox3_MouseDown;
+            pictureBox3.MouseUp += pictureBox3_MouseUp;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.White;
+            panel3.Controls.Add(textBox1);
+            panel3.Controls.Add(pictureBox2);
+            panel3.Location = new Point(0, 185);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(450, 45);
+            panel3.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.ForeColor = Color.FromArgb(95, 49, 96);
+            textBox1.Location = new Point(55, 11);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(370, 20);
+            textBox1.TabIndex = 5;
+            textBox1.Click += textBox1_Click;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = Properties.Resources._1564534_customer_man_user_account_profile_icon;
+            pictureBox2.Location = new Point(15, 11);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(24, 24);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 5;
+            pictureBox2.TabStop = false;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Century Gothic", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label6.ForeColor = Color.FromArgb(95, 49, 96);
+            label6.Location = new Point(38, 135);
+            label6.Name = "label6";
+            label6.Size = new Size(136, 24);
+            label6.TabIndex = 2;
+            label6.Text = "Iniciar Sesion";
+            // 
+            // cerrarbtn
+            // 
+            cerrarbtn.Cursor = Cursors.Hand;
+            cerrarbtn.FlatAppearance.BorderSize = 0;
+            cerrarbtn.FlatStyle = FlatStyle.Flat;
+            cerrarbtn.Font = new Font("Verdana", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            cerrarbtn.ForeColor = Color.FromArgb(95, 49, 96);
+            cerrarbtn.Location = new Point(410, 0);
+            cerrarbtn.Name = "cerrarbtn";
+            cerrarbtn.Size = new Size(40, 40);
+            cerrarbtn.TabIndex = 0;
+            cerrarbtn.Text = "X";
+            cerrarbtn.UseVisualStyleBackColor = true;
+            cerrarbtn.Click += cerrarbtn_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(750, 530);
+            ControlBox = false;
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            panel4.ResumeLayout(false);
+            panel4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private PictureBox pictureBox1;
+        private Panel panel2;
+        private Label label4;
+        private Label label5;
+        private Button cerrarbtn;
+        private Button button1;
+        private Panel panel4;
+        private TextBox textBox2;
+        private PictureBox pictureBox3;
+        private Panel panel3;
+        private TextBox textBox1;
+        private PictureBox pictureBox2;
+        private Label label6;
+        private LinkLabel linkLabel1;
+        private Label label9;
+        private Label label8;
+        private Label label7;
+    }
+}
---
+++ ALLQOVET/Form1.cs
@@ -0,0 +1,54 @@
+using System.Drawing;
+
+namespace ALLQOVET
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cerrarbtn_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            textBox1.BackColor = Color.White;
+            panel3.BackColor = Color.White;
+            panel4.BackColor = SystemColors.Control;
+            textBox2.BackColor = SystemColors.Control;
+        }
+
+        private void textBox2_Click(object sender, EventArgs e)
+        {
+            textBox2.BackColor = Color.White;
+            panel4.BackColor = Color.White;
+
+            textBox1.BackColor = SystemColors.Control;
+            panel3.BackColor = SystemColors.Control;
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_MouseDown(object sender, MouseEventArgs e)
+        {
+            textBox2.UseSystemPasswordChar = false;
+        }
+
+        private void pictureBox3_MouseUp(object sender, MouseEventArgs e)
+        {
+            textBox2.UseSystemPasswordChar = true;
+        }
+    }
+}
