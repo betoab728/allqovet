56ef43c23f1ae4c60a4fa7b434d17996f86ff660
EstefanoSeminario <seminarioaugusto08@gmail.com>
2023-05-16 00:35:27 -0500

    PRUEBA 4

--- ALLQOVET/frmMarcas.Designer.cs
+++ ALLQOVET/frmMarcas.Designer.cs
@@ -30,6 +30,9 @@
         {
             button1 = new Button();
             label1 = new Label();
+            button2 = new Button();
+            checkedListBox1 = new CheckedListBox();
+            monthCalendar1 = new MonthCalendar();
             SuspendLayout();
             // 
             // button1
@@ -50,11 +53,37 @@
             label1.TabIndex = 1;
             label1.Text = "MARCAS";
             // 
+            // button2
+            // 
+            button2.Location = new Point(178, 104);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 2;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // checkedListBox1
+            // 
+            checkedListBox1.FormattingEnabled = true;
+            checkedListBox1.Location = new Point(459, 64);
+            checkedListBox1.Name = "checkedListBox1";
+            checkedListBox1.Size = new Size(120, 94);
+            checkedListBox1.TabIndex = 3;
+            // 
+            // monthCalendar1
+            // 
+            monthCalendar1.Location = new Point(255, 104);
+            monthCalendar1.Name = "monthCalendar1";
+            monthCalendar1.TabIndex = 4;
+            // 
             // frmMarcas
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(monthCalendar1);
+            Controls.Add(checkedListBox1);
+            Controls.Add(button2);
             Controls.Add(label1);
             Controls.Add(button1);
             Name = "frmMarcas";
@@ -67,5 +96,8 @@
 
         private Button button1;
         private Label label1;
+        private Button button2;
+        private CheckedListBox checkedListBox1;
+        private MonthCalendar monthCalendar1;
     }
 }
