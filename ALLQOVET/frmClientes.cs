d0c8553425a059082ac17fe9815fa152f6111239
unknown <seminarioaugusto08@gmail.com>
2023-05-15 23:58:21 -0500

    Prueba 1

--- ALLQOVET/frmClientes.cs
+++ ALLQOVET/frmClientes.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace ALLQOVET
 {
-    public partial class Form1 : Form
+    public partial class frmClientes : Form
     {
-        public Form1()
+        public frmClientes()
         {
             InitializeComponent();
         }
