f825fa02c76b1b6fb82391fbaa3ed75408223cf4
betoab728 <betoab728@gmail.com>
2023-05-16 00:38:50 -0500

    prueba 5

--- ALLQOVET/frmMarcas.Designer.cs
+++ ALLQOVET/frmMarcas.Designer.cs
@@ -33,6 +33,13 @@
             button2 = new Button();
             checkedListBox1 = new CheckedListBox();
             monthCalendar1 = new MonthCalendar();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button3 = new Button();
+            groupBox1 = new GroupBox();
+            groupBox1.SuspendLayout();
             SuspendLayout();
             // 
             // button1
@@ -72,15 +79,71 @@
             // 
             // monthCalendar1
             // 
-            monthCalendar1.Location = new Point(255, 104);
+            monthCalendar1.Location = new Point(624, 47);
             monthCalendar1.Name = "monthCalendar1";
             monthCalendar1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(21, 25);
+            label2.Name = "label2";
+            label2.Size = new Size(98, 15);
+            label2.TabIndex = 5;
+            label2.Text = "Nommbre marca";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(32, 86);
+            label3.Name = "label3";
+            label3.Size = new Size(73, 15);
+            label3.TabIndex = 6;
+            label3.Text = "Descripciom";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(125, 22);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(158, 23);
+            textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(125, 86);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(145, 23);
+            textBox2.TabIndex = 8;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(75, 127);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 9;
+            button3.Text = "Guardar";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(button3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Location = new Point(459, 327);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(306, 178);
+            groupBox1.TabIndex = 10;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Marcas";
             // 
             // frmMarcas
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1152, 559);
+            Controls.Add(groupBox1);
             Controls.Add(monthCalendar1);
             Controls.Add(checkedListBox1);
             Controls.Add(button2);
@@ -88,6 +151,8 @@
             Controls.Add(button1);
             Name = "frmMarcas";
             Text = "frmMarcas";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -99,5 +164,11 @@
         private Button button2;
         private CheckedListBox checkedListBox1;
         private MonthCalendar monthCalendar1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button3;
+        private GroupBox groupBox1;
     }
 }
